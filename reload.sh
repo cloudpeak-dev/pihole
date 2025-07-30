b6c3dddec53d9c358a87472a469bd900e0fc5c56
Rokas Kasperavicius <github.rokaskasperavicius@gmail.com>
2025-07-31 00:46:59 +0200

    extra bash to set log permissions

--- reload.sh
+++ reload.sh
@@ -4,3 +4,8 @@ git pull origin main
 
 echo "Reloading docker container..."
 sudo docker compose -f ./docker-compose.yaml up -d
+
+echo "Setting correct log owner..."
+sudo chgrp -R adm /var/log/pihole
+sudo chmod 750 /var/log/pihole
+sudo chmod 640 /var/log/pihole/*.log
