1f1a3b820e7c569b8a5fe0089530004d37a86afb
Rokas Kasperavicius <github.rokaskasperavicius@gmail.com>
2025-07-31 22:16:56 +0200

    testing host.docker.internal

--- reload.sh
+++ reload.sh
@@ -6,6 +6,10 @@ echo "Reloading docker container..."
 sudo docker compose -f ./docker-compose.yaml up -d
 
 echo "Setting correct log owner..."
+
+# Set the group of the file/directory to adm.
+# This allows alloy to access the logs.
 sudo chgrp -R adm /var/log/pihole
+# Set directory permissions
 sudo chmod 750 /var/log/pihole
 sudo chmod 640 /var/log/pihole/*.log
